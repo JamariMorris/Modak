53c1fdecb72c3f5d741a1f8d632a15730452a5e6
Jamari L Morris <jamarimorris7@gmail.com>
2022-10-10 20:29:03 -0400

    Fully deployed product

--- Modak/AppDelegate.swift
+++ Modak/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Parse
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "RxYQ6GmTWjlvb7942cHgsVq2Wy6QwmWC2WpLyEEl" // <- UPDATE
+                    $0.clientKey = "NJCCbiRYVkbiSK3nBm5WCkGOiJXamtrjxZfQTq2P" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Modak' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Modak
+  pod 'AlamofireImage'
+  pod 'Parse'
+
+  target 'ModakTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ModakUITests' do
+    # Pods for testing
+  end
+
+end
